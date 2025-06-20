a3a30dbd279e6c96a214214d66b460c3b52d3f36
abhishekkamble04 <abhishekkamble.sits.comp@gmail.com>
2025-06-20 12:46:31 +0530

    new d

--- pt.java
+++ pt.java
@@ -1,6 +1,6 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int num1 = 10; // First number
+        int num1 = 50; // First number
         int num2 = 20; // Second number
 
         int sum = num1 + num2; // Addition
